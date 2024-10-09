d0b1708233baddb57e2df1f4d9bdfea5a4f17fb3
Dana Ng <ngdana1@gmail.com>
2024-10-08 21:49:00 -0700

    add shim

---
+++ src/components/AudioVisual.vue.js
@@ -0,0 +1,82 @@
+export default (await import('vue')).defineComponent({
+    name: 'AudioVisual'
+});
+function __VLS_template() {
+    const __VLS_ctx = {};
+    const __VLS_localComponents = {
+        ...{},
+        ...{},
+        ...__VLS_ctx,
+    };
+    let __VLS_components;
+    const __VLS_localDirectives = {
+        ...{},
+        ...__VLS_ctx,
+    };
+    let __VLS_directives;
+    let __VLS_styleScopedClasses;
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    __VLS_styleScopedClasses['ring'];
+    __VLS_styleScopedClasses['inner-circle'];
+    // CSS variable injection 
+    // CSS variable injection end 
+    let __VLS_resolvedLocalAndGlobalComponents;
+    __VLS_elementAsFunction(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)({ ...{ class: ("main-circle") }, });
+    for (const [ring] of __VLS_getVForSourceType((3))) {
+        __VLS_elementAsFunction(__VLS_intrinsicElements.div, __VLS_intrinsicElements.div)({ ...{ class: ("ring") }, key: ((ring)), });
+    }
+    __VLS_styleScopedClasses['main-circle'];
+    __VLS_styleScopedClasses['ring'];
+    var __VLS_slots;
+    var __VLS_inheritedAttrs;
+    const __VLS_refs = {};
+    var $refs;
+    return {
+        slots: __VLS_slots,
+        refs: $refs,
+        attrs: {},
+    };
+}
+;
+let __VLS_self;
